cf0cf86c8939b65521144a61ffe712472a539de2
QcHsGUNNER22 <oliviertailly@hotmail.com>
2025-04-18 17:49:20 -0400

    page eclipse

--- generer_galerie_json.py
+++ generer_galerie_json.py
@@ -9,13 +9,14 @@ galeries = {
     "LUNE": [],
     "PLANETES": [],
     "SOLEIL": [],
+    "eclipse solaire": [],
 }
 
 for galerie in galeries:
     folder = os.path.join(base_path, galerie)
     if os.path.exists(folder):
         for file in sorted(os.listdir(folder)):
-            if file.lower().endswith((".webp", ".jpg", ".jpeg", ".png")):
+            if file.lower().endswith((".webp", ".jpg", ".jpeg", ".png", ".mp4")):
                 image_path = os.path.join(base_path, galerie, file).replace("\\", "/")
                 galeries[galerie].append({
                     "src": image_path,
